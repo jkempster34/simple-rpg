80ed28ba87f83723b3a06aecdd2ebddf30290471
Joseph KEMPSTER <Joseph.KEMPSTER@EDUCATION.GOV.UK>
2020-01-30 12:39:17 +0000

    Add functionality whereby player can move around game world.

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -11,7 +11,7 @@ namespace Engine.Factories
         {
             World newWorld = new World();
 
-            newWorld.AddLocation(-1, -1, "Farmer's Field",
+            newWorld.AddLocation(-2, -1, "Farmer's Field",
                 "There are rows of corn growing here, with giant rats hiding between them",
                 "C:/Users/jkempster/OneDrive - Department for Education/Desktop/SOSCSRPG/Engine/Images/Locations/FarmFields.png");
 
