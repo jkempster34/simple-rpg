d2064ea64071e7ef84a7313a8f785b4e42588d46
Joseph Kempster <jkempster34@gmail.com>
2020-01-31 12:50:40 +0000

    Add game items

---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamaage) : base(itemTypeID, name, price)
+        {
+            MinimumDamage = minDamage;
+            MaximumDamage = maxDamaage;
+        }
+    }
+}
